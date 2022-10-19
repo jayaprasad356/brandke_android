6341e55cbf9f84da35b2ca045756bc78616ac9a5
jayaprasad356 <jayaprasad356@gmail.com>
2022-10-19 16:01:33 +0530

    bug fixed

--- app/src/main/java/com/greymatter/brandke/MainActivity.java
+++ app/src/main/java/com/greymatter/brandke/MainActivity.java
@@ -7,13 +7,12 @@ import androidx.fragment.app.FragmentManager;
 import android.os.Bundle;
 import android.widget.FrameLayout;
 
-import com.ismaeldivita.chipnavigation.ChipNavigationBar;
 
 public class MainActivity extends AppCompatActivity {
 
     public static FragmentManager fm;
     private Fragment HomeFrag,FinTechFrag,TokenFrag,WalletFrag,ProfileFrag;
-    private ChipNavigationBar chipNavigationBar;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,46 +26,8 @@ public class MainActivity extends AppCompatActivity {
         WalletFrag = new WalletFragment();
         ProfileFrag = new ProfileFragment();
 
-        chipNavigationBar = findViewById(R.id.NavigationView);
-        fm.beginTransaction().replace(R.id.FrameLyt,HomeFrag).addToBackStack("null").commit();
-        chipNavigationBar.setItemSelected(R.id.Home,true,true);
 
-        chipNavigationBar.setOnItemSelectedListener(new ChipNavigationBar.OnItemSelectedListener() {
-            @Override
-            public void onItemSelected(int i) {
-                switch (i) {
-                    case R.id.Home:
-                        fm.beginTransaction()
-                                .replace(R.id.FrameLyt,HomeFrag)
-                                .addToBackStack("null")
-                                .commit();
-                        break;
-                    case R.id.Fintech:
-                        fm.beginTransaction()
-                                .replace(R.id.FrameLyt,FinTechFrag)
-                                .addToBackStack("null")
-                                .commit();
-                        break;
-                    case R.id.Token:
-                        fm.beginTransaction()
-                                .replace(R.id.FrameLyt,TokenFrag)
-                                .addToBackStack("null")
-                                .commit();
-                        break;
-                    case R.id.Wallet:
-                        fm.beginTransaction()
-                                .replace(R.id.FrameLyt,WalletFrag)
-                                .addToBackStack("null")
-                                .commit();
-                        break;
-                    case R.id.Profile:
-                        fm.beginTransaction()
-                                .replace(R.id.FrameLyt,ProfileFrag)
-                                .addToBackStack("null")
-                                .commit();
-                        break;
-                }
-            }
-        });
+        fm.beginTransaction().replace(R.id.FrameLyt,HomeFrag).addToBackStack("null").commit();
+
     }
 }
