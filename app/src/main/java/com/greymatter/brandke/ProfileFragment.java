5300b9e5c24e806ada07ad01b0585816e17288d2
vishalaravind754 <vishalaravind754@gmail.com>
2022-10-21 17:42:50 +0530

    Profile Fargment updated

--- app/src/main/java/com/greymatter/brandke/ProfileFragment.java
+++ app/src/main/java/com/greymatter/brandke/ProfileFragment.java
@@ -1,5 +1,6 @@
 package com.greymatter.brandke;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,58 +8,85 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link ProfileFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import com.google.android.material.bottomsheet.BottomSheetDialog;
+
 public class ProfileFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    TextView order_txt;
+    TextView cart_txt;
+    TextView notification_txt;
+    TextView password_txt;
+    TextView wallet_txt;
+    TextView contact_txt;
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
 
     public ProfileFragment() {
-        // Required empty public constructor
-    }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment ProfileFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static ProfileFragment newInstance(String param1, String param2) {
-        ProfileFragment fragment = new ProfileFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false);
+        View rootview = inflater.inflate(R.layout.fragment_profile, container, false);
+
+        order_txt = rootview.findViewById(R.id.order_txt);
+        cart_txt = rootview.findViewById(R.id.cart_txt);
+        notification_txt = rootview.findViewById(R.id.notification_txt);
+        password_txt = rootview.findViewById(R.id.password_txt);
+        wallet_txt = rootview.findViewById(R.id.wallet_txt);
+        contact_txt = rootview.findViewById(R.id.contact_txt);
+
+        order_txt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(),OrderActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        cart_txt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(),CartActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        notification_txt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(),NotificationActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        password_txt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                BottomSheetDialog bottomSheetDialog = new BottomSheetDialog(getActivity());
+                bottomSheetDialog.setContentView(R.layout.bottom_password_change);
+            }
+        });
+
+        wallet_txt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(),WalletActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        contact_txt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(),contactusActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        return rootview;
     }
 }
