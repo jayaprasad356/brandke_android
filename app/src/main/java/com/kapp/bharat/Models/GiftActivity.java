f8c0fcfec6a018bee731b944095ba4c32ec67614
chhagan <chhaganswami1@gmail.com>
2023-01-02 20:47:05 +0530

    mycommit

---
+++ app/src/main/java/com/kapp/bharat/Models/GiftActivity.java
@@ -0,0 +1,16 @@
+package com.kapp.bharat.Models;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.kapp.bharat.R;
+
+public class GiftActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_gift);
+    }
+}
